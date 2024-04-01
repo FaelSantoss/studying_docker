4df7a9843c590eefe5abe57be73fe7754529476c
FaelSantoss <silva.faelm@gmail.com>
2024-03-31 23:49:24 -0300

    connection

---
+++ src/config/config.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+class DBConnection:
+    def __init__(self) -> None:
+        self.__connection_string = 'mysql+pymysql://root:123@mysqldb/teste'
+        self.session = None
+
+    def __enter__(self):
+        engine = create_engine(self.__connection_string)
+        session_maker = sessionmaker()
+        self.session = sessionmaker(bind=engine)
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
